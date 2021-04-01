23fb5d70bf9466a48bd651ebe9d33c4c48791d5a
notoriousturtle <notoriousturtle@users.noreply.github.com>
2021-04-01 20:01:12 +1100

    Update new_install_extras_post_install.sh

--- new_install_extras_post_install.sh
+++ new_install_extras_post_install.sh
@@ -3,3 +3,5 @@ pip install py-altdns
 sudo apt install whatweb
 sudo snap install amass
 GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+go get -u github.com/tomnomnom/assetfinder
